5a510d295e98fe1ffa45169028d7f703135bfbbb
Ryan Eldho <reldho@umd.edu>
2025-04-12 06:43:52 -0400

    added pages skeletons

---
+++ piggy/src/pages/Piggy.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+function Piggy() {
+  return (
+    <div className="Piggy">
+    </div>
+  );
+}
+
+export default Piggy;
---
+++ piggy/src/pages/Settings.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+function Settings() {
+  return (
+    <div className="Settings">
+    </div>
+  );
+}
+
+export default Settings;
