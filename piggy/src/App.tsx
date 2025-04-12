df0071f946a62502f2bca0f7e640cf3fb776330f
Ryan Eldho <reldho@umd.edu>
2025-04-12 08:33:30 -0400

    converted to material UI

--- piggy/src/App.tsx
+++ piggy/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import './App.css';
+import './css/App.css';
 import TestDashboard from './components/TestDashboard';
+import Piggy from './pages/Piggy';
 
 const accountId = "67f9f6fb9683f20dd5194d5c"
 const testMerchantIkea = "66ef43749683f20dd518a511"
@@ -8,8 +9,8 @@ const testMerchantIkea = "66ef43749683f20dd518a511"
 function App() {
   return (
     <div className="App">
-      <h1>Dashboard</h1>
-      <TestDashboard userId = {accountId}/>
+      {/* <TestDashboard userId = {accountId}/> */}
+      <Piggy/>
     </div>
   );
 }
--- piggy/src/pages/Piggy.tsx
+++ piggy/src/pages/Piggy.tsx
@@ -1,11 +1,81 @@
-import React from 'react';
-import './App.css';
+import { Typography, Button, Box } from '@mui/material';
+import settings from '../assets/settings.png';
+import piggyImage from '../assets/piggy.png';
+import '../css/piggy.css';
 
-function Piggy() {
+export default function Piggy() {
   return (
-    <div className="Piggy">
-    </div>
+    <Box className="border">
+      <Header />
+      <Typography variant="h4" id="summaryTitle" >
+        Summary for April
+      </Typography>
+
+      <Box className="container">
+        <Summary />
+        <ImageAndBar />
+        <PiggyActions />
+      </Box>
+    </Box>
   );
 }
 
-export default Piggy;
+
+function Header() {
+  return (
+    <Box className="header">
+      <Typography variant="h5" id="welcome">
+        Hello Ryan
+      </Typography>
+      <img src={settings} id="settings" alt="Settings" />
+    </Box>
+  );
+}
+
+function Summary() {
+  return (
+    <Box className="summary">
+      <Box className="summaryDetails">
+        <Typography variant="subtitle1" id="earningsMade">
+          Earnings: <br /> $0.00
+        </Typography>
+        <Typography variant="subtitle1" id="moneySpent">
+          Spent: <br /> $500.00
+        </Typography>
+      </Box>
+      <Box className="summaryDetails">
+        <Typography variant="subtitle1" id="budget">
+          Budget: <br /> $1000.00
+        </Typography>
+        <Typography variant="subtitle1" id="netChange">
+          Change: <br /> -$500.00
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
+
+function ImageAndBar() {
+  return (
+    <Box className="piggy-container">
+      <img src={piggyImage} id="piggyBank" alt="Piggy Bank" />
+      <div className="fill-bar" id="fillBar"></div>
+    </Box>
+  );
+}
+
+function PiggyActions() {
+  return (
+    <Box className="buttons" display="flex" gap={3} justifyContent="center" mt={2}>
+      <Button
+        variant="contained"
+        sx={{ backgroundColor: '#f6a91a', color: '#fff'}}
+      >
+        Calculate
+      </Button>
+      <Button variant="outlined">
+        Expand Transactions
+      </Button>
+    </Box>
+  );
+}
