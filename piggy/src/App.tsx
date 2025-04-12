10c325906cb91718441e42d1ffa695b343a8b1cd
Akshay B <akshaymahadev04@gmail.com>
2025-04-12 12:45:04 -0400

    router

--- piggy/src/App.tsx
+++ piggy/src/App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import './css/App.css';
 import TestDashboard from './components/TestDashboard';
 import Piggy from './pages/Piggy';
+import Transactions from './pages/Transactions';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 const accountId = "67f9f6fb9683f20dd5194d5c"
 const testMerchantIkea = "66ef43749683f20dd518a511"
@@ -9,8 +12,15 @@ const testMerchantIkea = "66ef43749683f20dd518a511"
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Piggy />} />
+          <Route path="/Transactions" element={<Transactions />} />
+        </Routes>
+        <Piggy/>
+      </Router>
       {/* <TestDashboard userId = {accountId}/> */}
-      <Piggy/>
+      {/*<Transactions/>*/}
     </div>
   );
 }
---
+++ piggy/src/pages/PiggyToTransactions.tsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Typography, Button, Box } from '@mui/material';
+
+const PiggyToTransactions = () => {
+    const [clicked, setClicked] = useState<boolean>(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (clicked) {
+        navigate("/Transactions"); // Redirect after state update
+        }
+    }, [clicked, navigate]);
+
+    const handleClick = () => {
+        setClicked(true); // Set state on click
+    };
+  
+
+    return (
+      <Box className="buttons" display="flex" gap={3} justifyContent="center" mt={2}>
+        <Button
+          variant="contained"
+          sx={{ backgroundColor: '#f6a91a', color: '#fff'}}
+        >
+          Calculate
+        </Button>
+        <Button variant="outlined" onClick={handleClick}>
+          Expand Transactions
+        </Button>
+      </Box>
+    );
+  };
+  export default PiggyToTransactions;
