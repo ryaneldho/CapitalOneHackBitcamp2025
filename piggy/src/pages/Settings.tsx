d7c5f315d0bcb9a643c17065861fb4bf0f57a3b1
Tiffany Lu <tiffanylu123901@gmail.com>
2025-04-12 18:58:42 -0400

    made the settings page prettier

--- piggy/src/pages/Settings.tsx
+++ piggy/src/pages/Settings.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import '../css/settings.css';
 import { Typography, Button, Box, FormGroup, Switch, FormControlLabel, TextField } from '@mui/material';
+import backButton from '../assets/backButton.png'
 
 interface SettingsProps {
   switchStates: SwitchStates;
@@ -49,13 +50,19 @@ function Settings({ switchStates, setSwitchStates, budgetValue, setBudgetValue }
   
   return (
     <Box className="border">
-      <Box className="header">
-        <Typography variant="h4" id="title">
+      <Box className="header" sx={{height:'64px'}}>
+        <img src={backButton} id="backButton" alt="backButton" sizes="small" onClick={() => setClicked(true)}
+         style={{
+          height: '50px',
+          position: 'absolute',
+          left: '16px',
+          cursor: 'pointer',
+          width: '50px',
+        }}/>
+        <Typography variant="h4" id="title" style={{fontSize:'20px'}}>
           Settings
         </Typography>
-          <Button variant="outlined" color="error" sx={{ mb: 2 }} onClick={() => setClicked(true)}>
-                GO BACK
-          </Button>
+          
       </Box>
       <Box>
         <Budget budgetValue={budgetValue} setBudgetValue={setBudgetValue}/>
@@ -85,8 +92,39 @@ function Budget({ budgetValue, setBudgetValue }: BudgetProps){
   };
 
   return (
-    <TextField id="budget" required label="Enter your budget" variant="outlined" value={budgetValue}
-    onChange={handleChange} type="number" sx={{transform: 'scale(1.5)', margin: 1}} />
+    <Box sx={{height:'140px'}}>
+      <Typography variant = "h4" id = "subHeaders" style={{fontSize:'23px', left: '16px', textAlign: 'left', marginLeft: '16px'}}>
+        Budget Goals
+      </Typography>
+      <TextField id="budget" required label="Enter your budget" variant="outlined" value={budgetValue}
+      onChange={handleChange} type="number" sx={{transform: 'scale(1.5)',  height: '40px', //sets height for the whole input field
+        '& input': {
+          padding: '14px 14px', // controls inside text padding
+        }, margin:4,
+        left: '-45px',
+        backgroundColor: '#6f655b',
+    '& .MuiOutlinedInput-root': {
+      backgroundColor: 'white', // background inside the input
+    },
+    '& fieldset': {
+        borderColor: '#E9CA93',
+      },
+      '&:hover fieldset': {
+        borderColor: '#E9CA93',
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: '#E9CA93',
+      },
+    
+    '& .MuiInputLabel-root': {
+      color: 'black', // label color
+    },
+    '& .MuiInputLabel-root.Mui-focused': {
+      color: 'black', // label when focused
+    },
+      }} />
+    </Box>
+  
   );
 }
 
@@ -111,31 +149,42 @@ function Options({ switchStates, setSwitchStates }: OptionsProps) {
   } = switchStates;
 
   return (
-  <FormGroup className="options">
-    <FormControlLabel id="deposits" control={<Switch defaultChecked id="deposits" checked={switchStates.deposits} onChange={handleChange} size="medium" 
-     sx={{transform: 'scale(1.5)', margin: 1}}/>} 
-    label="Deposits" />
-    <br/>
-    <FormControlLabel id="withdrawals" control={<Switch defaultChecked id="withdrawals" checked={switchStates.withdrawals} onChange={handleChange} size="medium"  
-    sx={{transform: 'scale(1.5)', margin: 1}} />}
-    label="Withdrawals" />
-     <br/>
-    <FormControlLabel id="purchase" control={<Switch defaultChecked id="purchase" checked={switchStates.purchase} onChange={handleChange} size="medium" 
-     sx={{transform: 'scale(1.5)', margin: 1}}/>}
-    label="Purchase" />
-     <br/>
-    <FormControlLabel id="loans" control={<Switch defaultChecked id="loans" checked={switchStates.loans} onChange={handleChange} size="medium" 
-     sx={{transform: 'scale(1.5)', margin: 1}}/>}
-    label="Loans" />
-     <br/>
-    <FormControlLabel id="transfers" control={<Switch defaultChecked id="transfers" checked={switchStates.transfers} onChange={handleChange} size="medium" 
-     sx={{transform: 'scale(1.5)', margin: 1}}/>}
-    label="Transfers" />
-     <br/>
-    <FormControlLabel id="bills" control={<Switch defaultChecked id="bills" checked={switchStates.transfers} onChange={handleChange} size="medium" 
-     sx={{transform: 'scale(1.5)', margin: 1}}/>}
-    label="Bills" />
-</FormGroup>
+    <Box>
+       <Typography variant = "h4" id = "subHeaders" style={{fontSize:'23px', left: '16px', marginTop:'20px', textAlign: 'left', marginLeft: '16px' }}>
+        Budget Customization
+      </Typography>
+          <FormGroup className="options" sx={{margin: 4,  '& .MuiSwitch-switchBase.Mui-checked': {
+              color: '#e0a53f',
+            },
+            '& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track': {
+            backgroundColor: '#c8ab7a',
+            },}}>
+            <FormControlLabel id="deposits" control={<Switch defaultChecked id="deposits" checked={switchStates.deposits} onChange={handleChange} size="medium" 
+            sx={{transform: 'scale(1.5)', margin: 1}}/>} 
+            label="Deposits" />
+            <br/>
+            <FormControlLabel id="withdrawals" control={<Switch defaultChecked id="withdrawals" checked={switchStates.withdrawals} onChange={handleChange} size="medium"  
+            sx={{transform: 'scale(1.5)', margin: 1}} />}
+            label="Withdrawals" />
+            <br/>
+            <FormControlLabel id="purchase" control={<Switch defaultChecked id="purchase" checked={switchStates.purchase} onChange={handleChange} size="medium" 
+            sx={{transform: 'scale(1.5)', margin: 1}}/>}
+            label="Purchase" />
+            <br/>
+            <FormControlLabel id="loans" control={<Switch defaultChecked id="loans" checked={switchStates.loans} onChange={handleChange} size="medium" 
+            sx={{transform: 'scale(1.5)', margin: 1}}/>}
+            label="Loans" />
+            <br/>
+            <FormControlLabel id="transfers" control={<Switch defaultChecked id="transfers" checked={switchStates.transfers} onChange={handleChange} size="medium" 
+            sx={{transform: 'scale(1.5)', margin: 1}}/>}
+            label="Transfers" />
+            <br/>
+            <FormControlLabel id="bills" control={<Switch defaultChecked id="bills" checked={switchStates.transfers} onChange={handleChange} size="medium" 
+            sx={{transform: 'scale(1.5)', margin: 1}}/>}
+            label="Bills" />
+        </FormGroup>
+    </Box>
+  
   )
 }
 
