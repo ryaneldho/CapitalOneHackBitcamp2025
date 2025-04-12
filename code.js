0bcf1a435284870086bb5b0351682f854e8f34cb
Akshay B <akshaymahadev04@gmail.com>
2025-04-12 03:51:44 -0400

    HTML/CSS changes so progress bar works now

---
+++ code.js
@@ -0,0 +1,18 @@
+function updateFillBar() {
+  let spent = document.getElementById("moneySpent").textContent;
+  let budget = document.getElementById("budget").textContent;
+
+  let dollarIndexSpent = spent.indexOf("$");
+  let amountSpent = Number(spent.slice(dollarIndexSpent + 1));
+
+  let dollarIndexBudget = budget.indexOf("$");
+  let amountBudget = Number(budget.slice(dollarIndexBudget + 1));
+
+  const fillBar = document.getElementById("fillBar");
+  const percent = (amountSpent / amountBudget) * 100;
+  fillBar.style.width = `${percent}%`;
+}
+
+function goToExpandTransactions() {
+    window.location.href = "transactions.html";
+}
